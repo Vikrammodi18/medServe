04f29a794a6020fc87a69ac3d1427338815accb2
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-05-24 16:50:17 +0530

    Add admin verification to uploadMedicine route

--- src/routes/medicine.route.js
+++ src/routes/medicine.route.js
@@ -3,8 +3,8 @@ const {Router} = require('express')
 const router = Router()
 const upload = require('../middleware/multer.middlware.js')
 const {uploadMedicine,getAllMedicine} = require('../controllers/medicine.controller.js')
-const {verifyJWT} = require('../middleware/auth.middleware.js')
+const {verifyJWT, verifyJWTAdmin} = require('../middleware/auth.middleware.js')
 
-router.route("/uploadMedicine").post(upload.single("medicineImage"),uploadMedicine)
+router.route("/uploadMedicine").post(verifyJWTAdmin,upload.single("medicineImage"),uploadMedicine)
 router.route("/getAllMedicine").get(getAllMedicine)
 module.exports = router
